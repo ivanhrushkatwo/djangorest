84d375acf7c7a36f10de2aad9c05c8d6a40cf307
ivanhrushka <ivanhrushka.py@gmail.com>
2017-02-14 22:13:01 +0200

    Init project

---
+++ api/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from rest_framework import generics
+
+from .serializers import BucketlistSerializer
+from .models import Bucketlist
+
+
+class CreateView(generics.ListCreateAPIView):
+
+    queryset = Bucketlist.objects.all()
+    serializer_class = BucketlistSerializer
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+
+class DetailsView(generics.RetrieveUpdateDestroyAPIView):
+    """This class handles the http GET, PUT and DELETE requests."""
+
+    queryset = Bucketlist.objects.all()
+    serializer_class = BucketlistSerializer
